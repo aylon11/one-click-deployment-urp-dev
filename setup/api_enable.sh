d96e4706a4304f5eb9aa48fe9efc45011435e0c1
Eylon Goor <eylong@google.com>
2022-06-20 13:07:33 +0300

    add api enable script and web hook

---
+++ setup/api_enable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Enable Ads API
+echo "Enabling Google Ads API..."
+gcloud services enable googleads.googleapis.com
+
+OAUTH_CALLBACK_URL=${SERVICE_URL}/oauth2callback
+
+echo "Add the following URL to your OAuth Client ID's 'Authorized redirect URIs':" ${OAUTH_CALLBACK_URL}
