b417f50424ce8d5c68291fcac1f2c1eb9c350dcb
Eylon Goor <aylong11@gmail.com>
2022-10-30 17:05:13 +0200

    enable storage service

--- setup/setup_cloud_storage.sh
+++ setup/setup_cloud_storage.sh
@@ -1,10 +1,13 @@
 echo "Setting Project ID: ${GOOGLE_CLOUD_PROJECT}"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 
-echo "Creating cloud storage bucket"
+echo "Enabling Cloud Storage service..."
+gcloud services enable storage-component.googleapis.com
+
+echo "Creating cloud storage bucket..."
 gcloud storage buckets create gs://one-click-deployment --project=${GOOGLE_CLOUD_PROJECT}
 
-echo "Creating google-ads.yaml file"
+echo "Creating google-ads.yaml file..."
 echo "
 client_customer_id: ${MCC_ID}
 client_id: ${OAUTH_CLIENT_ID}
@@ -13,5 +16,5 @@ refresh_token: ${REFRESH_TOKEN}
 developer_token: ${DEVELOPER_TOKEN}
 " >> google-ads.yaml
 
-echo "Writing google-ads.yaml file to cloud storage bucket"
+echo "Writing google-ads.yaml file to cloud storage bucket..."
 gcloud storage cp ./google-ads.yaml gs://one-click-deployment
